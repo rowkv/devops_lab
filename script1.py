5afe9cfba651ab37de0d65295403c345e79d561d
Raman Rohau <raman.rohau@epam.com>
2021-02-25 23:31:34 +0300

    Homework v.0.1

--- script1.py
+++
@@ -1,9 +0,0 @@
-def gregCheck(year):
-    if year % 100 == 0 and year % 400 != 0:
-        print('False')
-    elif year % 4 == 0:
-        print('True')
-
-
-year = int(input())
-gregCheck(year)
--- script2.py
+++
@@ -1,7 +0,0 @@
-def comSort(l1, l2):
-    return list(set(l1) & set(l2))
-
-
-l1 = [int(x) for x in input().split()]
-l2 = [int(x) for x in input().split()]
-print(*comSort(l1, l2))
