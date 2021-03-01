6f02fb1a6981f7dadf69f68eac257b459561808f
Raman Rohau <raman.rohau@epam.com>
2021-03-01 16:52:22 +0300

    Homework5 v.0.2

--- handlers/pulls.py
+++ handlers/pulls.py
@@ -5,16 +5,20 @@ def get_pulls(state):
     pulls = list()
     if state in ('accepted', 'needs work'):
         payload = {'per_page': 100}
-        response = requests.get('https://api.github.com/repos/alenaPy/devops_lab/pulls', params=payload)
+        response = requests.get('https://api.github.com/repos/alenaPy/devops_lab/pulls',
+                                params=payload)
         for item in response.json():
             try:
                 if item['labels'][0]['name'] == state:
-                    pulls.append({'num': item['number'], 'title': item['title'], 'link': item['url']})
+                    pulls.append({'num': item['number'],
+                                  'title': item['title'],
+                                  'link': item['url']})
             except IndexError:
                 continue
     elif state in ('open', 'closed'):
         payload = {'state': state, 'per_page': 100}
-        response = requests.get('https://api.github.com/repos/alenaPy/devops_lab/pulls', params=payload)
+        response = requests.get('https://api.github.com/repos/alenaPy/devops_lab/pulls',
+                                params=payload)
         for item in response.json():
             pulls.append({'num': item['number'], 'title': item['title'], 'link': item['url']})
     return pulls
